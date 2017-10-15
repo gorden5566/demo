f31507cf9bb4f49d78e379c04cf4313c95758df6
gorden5566 <gorden5566@126.com>
2017-10-15 17:50:13 +0800

    oom error demo

---
+++ src/main/java/com/gorden5566/demos/jvm/JavaVMStackOOM.java
@@ -0,0 +1,30 @@
+package com.gorden5566.demos.jvm;
+
+/**
+ * VM Args: -Xss2M
+ * 注意: 请谨慎运行，可能会导致主机卡死
+ */
+public class JavaVMStackOOM {
+
+    private void dontStop() {
+        while (true) {
+        }
+    }
+
+    public void stackLeakByThread() {
+        while (true) {
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    dontStop();
+                }
+            });
+            thread.start();
+        }
+    }
+
+    public static void main(String[] args) throws Throwable{
+        JavaVMStackOOM oom = new JavaVMStackOOM();
+        oom.stackLeakByThread();
+    }
+}
