3eaf3dc17f095d1a3b33b72e53cafd2168b3c1b7
gorden5566 <gorden5566@126.com>
2017-10-18 22:57:31 +0800

    引用计数算法的缺陷

--- src/main/java/com/gorden5566/demos/jvm/ReferenceCountingGC.java
+++ src/main/java/com/gorden5566/demos/jvm/ReferenceCountingGC.java
@@ -2,6 +2,8 @@ package com.gorden5566.demos.jvm;
 
 /**
  * testGC() 方法执行后，objA 和 objB会不会被 GC 呢？
+ *
+ * VM Args: -XX:+PrintGCDetails
  */
 public class ReferenceCountingGC {
 
@@ -13,6 +15,10 @@ public class ReferenceCountingGC {
      * 这个成员属性的唯一意义就是占内存，以便能在 GC 日志中看清楚是否被回收过
      */
     private byte[] bigSize = new byte[2 * _1MB];
+
+    public static void main(String[] args) {
+        testGC();
+    }
 
     public static void testGC() {
         ReferenceCountingGC objA = new ReferenceCountingGC();
