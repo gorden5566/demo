c01bb391adb7d63da3b9432bd6aa868c4fca111f
gorden5566 <gorden5566@126.com>
2017-10-22 22:18:57 +0800

    长期存活的对象进入老年代

--- src/main/java/com/gorden5566/demos/jvm/MinorGC.java
+++ src/main/java/com/gorden5566/demos/jvm/MinorGC.java
@@ -26,8 +26,23 @@ public class MinorGC {
         allocation = new byte[4 * _1MB];
     }
 
+    /**
+     * VM Args: -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:+UseSerialGC
+     * -XX:MaxTenuringThreshold=1 -XX:+PrintTenuringDistribution
+     */
+    public static void testTenuringThreshold() {
+        byte[] allocation1, allocation2, allocation3;
+        allocation1 = new byte[_1MB / 4];
+        // 什么时候进入老年代取决于 -XX:MaxTenuringThreshold
+        allocation2 = new byte[4 * _1MB];
+        allocation3 = new byte[4 * _1MB];
+        allocation3 = null;
+        allocation3 = new byte[4 * _1MB];
+    }
+
     public static void main(String[] args) {
 //        testAllocation();
-        testPretenureSizeThreshold();
+//        testPretenureSizeThreshold();
+        testTenuringThreshold();
     }
 }
