7fc7f60706188f87955fac67b35de697661c837a
gorden5566 <gorden5566@126.com>
2017-10-22 20:04:17 +0800

    新生代 Minor GC

---
+++ src/main/java/com/gorden5566/demos/jvm/MinorGC.java
@@ -0,0 +1,23 @@
+package com.gorden5566.demos.jvm;
+
+/**
+ * Created by gorden5566 on 2017/10/22.
+ */
+public class MinorGC {
+    private static final int _1MB = 1024 * 1024;
+
+    /**
+     * VM Args: -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:+UseSerialGC
+     */
+    public static void testAllocation() {
+        byte[] allocation1, allocation2, allocation3, allocation4;
+        allocation1 = new byte[2 * _1MB];
+        allocation2 = new byte[2 * _1MB];
+        allocation3 = new byte[2 * _1MB];
+        allocation4 = new byte[4 * _1MB];   // 出现异常 Minor GC
+    }
+
+    public static void main(String[] args) {
+        testAllocation();
+    }
+}
