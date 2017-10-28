3a9a2764906a6103abd9c913e8ec312b067dabf0
gorden5566 <gorden5566@126.com>
2017-10-28 22:33:35 +0800

    动态对象年龄判定

--- src/main/java/com/gorden5566/demos/jvm/MinorGC.java
+++ src/main/java/com/gorden5566/demos/jvm/MinorGC.java
@@ -40,9 +40,25 @@ public class MinorGC {
         allocation3 = new byte[4 * _1MB];
     }
 
+    /**
+     * VM Args: -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:+UseSerialGC
+     * -XX:MaxTenuringThreshold=15 -XX:+PrintTenuringDistribution
+     */
+    public static void testTenuringThreshold2() {
+        byte[] allocation1, allocation2, allocation3, allocation4;
+        allocation1 = new byte[_1MB / 4];
+        // allocation1+allocation2 大于 survivor 空间一半
+        allocation2 = new byte[_1MB / 4];
+        allocation3 = new byte[4 * _1MB];
+        allocation4 = new byte[4 * _1MB];
+        allocation4 = null;
+        allocation4 = new byte[4 * _1MB];
+    }
+
     public static void main(String[] args) {
 //        testAllocation();
 //        testPretenureSizeThreshold();
-        testTenuringThreshold();
+//        testTenuringThreshold();
+        testTenuringThreshold2();
     }
 }
