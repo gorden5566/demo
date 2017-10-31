12f4a35d5f2c10575bec183b270da5f1bf8758cb
gorden5566 <gorden5566@126.com>
2017-10-31 22:29:12 +0800

    死锁

---
+++ src/main/java/com/gorden5566/demos/jvm/DeadLock.java
@@ -0,0 +1,31 @@
+package com.gorden5566.demos.jvm;
+
+/**
+ * Created by gorden5566 on 2017/10/31.
+ */
+public class DeadLock {
+    static class SynAddRunable implements Runnable {
+        int a, b;
+
+        public SynAddRunable(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        @Override
+        public void run() {
+            synchronized (Integer.valueOf(a)) {
+                synchronized (Integer.valueOf(b)) {
+                    System.out.println(a + b);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            new Thread(new SynAddRunable(1, 2)).start();
+            new Thread(new SynAddRunable(2, 1)).start();
+        }
+    }
+}
