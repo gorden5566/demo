c232ec72af9e6c28ad99c440aade00eb15f12f8b
gorden5566 <gorden5566@126.com>
2019-01-04 14:45:29 +0800

    HashMap并发问题测试

---
+++ src/main/java/com/gorden5566/base/TestHashMap.java
@@ -0,0 +1,36 @@
+package com.gorden5566.base;
+
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author gorden5566
+ * @date 2018/12/25
+ *
+ * Hash 并发问题
+ */
+public class TestHashMap extends Thread {
+    static HashMap<Integer, Integer> map = new HashMap<Integer, Integer>(2);
+    static AtomicInteger at = new AtomicInteger();
+
+    @Override
+    public void run() {
+        while (at.get() < 1000000) {
+            map.put(at.get(), at.get());
+            at.incrementAndGet();
+        }
+    }
+
+    public static void main(String[] args) {
+        TestHashMap t0 = new TestHashMap();
+        TestHashMap t1 = new TestHashMap();
+        TestHashMap t2 = new TestHashMap();
+        TestHashMap t3 = new TestHashMap();
+        TestHashMap t4 = new TestHashMap();
+        t0.start();
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+    }
+}
