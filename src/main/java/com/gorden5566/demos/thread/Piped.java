10f7226f5dc5a65f2676fcca584b6432c06f427a
gorden5566 <gorden5566@126.com>
2019-01-05 13:41:22 +0800

    管道输入输出

---
+++ src/main/java/com/gorden5566/demos/thread/Piped.java
@@ -0,0 +1,52 @@
+package com.gorden5566.demos.thread;
+
+import java.io.IOException;
+import java.io.PipedReader;
+import java.io.PipedWriter;
+
+/**
+ * @author gorden5566
+ * @date 2019/1/5
+ */
+public class Piped {
+    public static void main(String[] args) throws IOException {
+        PipedWriter out = new PipedWriter();
+        PipedReader in = new PipedReader();
+
+        // 将输出流和输入流进行连接，否则在使用时会抛出 IOException
+        out.connect(in);
+
+        Thread printThread = new Thread(new Print(in), "PrintThread");
+        printThread.start();
+
+        int receive = 0;
+        try {
+            while ((receive = System.in.read()) != -1) {
+                out.write(receive);
+            }
+        } finally {
+            out.close();
+        }
+    }
+
+    static class Print implements Runnable {
+        private PipedReader in;
+
+        public Print(PipedReader in) {
+            this.in = in;
+        }
+
+        @Override
+        public void run() {
+            int receive = 0;
+
+            try {
+                while ((receive = in.read()) != -1) {
+                    System.out.print((char) receive);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
