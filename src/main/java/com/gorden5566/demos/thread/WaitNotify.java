7c3529fffe57ce32fdfec6314c29b6d42615a16f
gorden5566 <gorden5566@126.com>
2019-01-05 11:49:54 +0800

    注释

--- src/main/java/com/gorden5566/demos/thread/WaitNotify.java
+++ src/main/java/com/gorden5566/demos/thread/WaitNotify.java
@@ -25,7 +25,9 @@ public class WaitNotify {
     static class Wait implements Runnable {
         @Override
         public void run() {
+            // 加锁，拥有 lock 的 Monitor
             synchronized (lock) {
+                // 当条件不满足时，继续 wait，同时释放了 lock 的锁
                 while (flag) {
                     try {
                         System.out.println(Thread.currentThread() + " flag is true. wait @ " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
@@ -35,7 +37,8 @@ public class WaitNotify {
                     }
                 }
 
-                System.out.println(Thread.currentThread() + " flag is false. wait @ " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
+                // 添加满足时，完成工作
+                System.out.println(Thread.currentThread() + " flag is false. running @ " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
             }
         }
     }
@@ -43,15 +46,19 @@ public class WaitNotify {
     static class Notify implements Runnable {
         @Override
         public void run() {
+            // 加锁，拥有 lock 的 Monitor
             synchronized (lock) {
+                // 获取 lock 的锁，然后进行通知，通知时不会释放 lock 的锁，
+                // 直到当前线程释放了 lock 后，WaitThread 才能从 wait 方法中返回
                 System.out.println(Thread.currentThread() + " hold lock. notify @ " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
                 lock.notifyAll();
                 flag = false;
                 SleepUtils.second(5);
             }
 
+            // 再次加锁
             synchronized (lock) {
-                System.out.println(Thread.currentThread() + " hold lock again. notify @ " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
+                System.out.println(Thread.currentThread() + " hold lock again. sleep @ " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
                 SleepUtils.second(5);
             }
         }
