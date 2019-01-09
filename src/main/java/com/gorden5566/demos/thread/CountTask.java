f5763f54f870aa1e5466d0d0d2da7090f8e78fd0
gorden5566 <gorden5566@126.com>
2019-01-09 17:31:31 +0800

    ForkJoin用法

---
+++ src/main/java/com/gorden5566/demos/thread/CountTask.java
@@ -0,0 +1,73 @@
+package com.gorden5566.demos.thread;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
+import java.util.concurrent.RecursiveTask;
+
+/**
+ * @author gorden5566
+ * @date 2019/1/9
+ */
+public class CountTask extends RecursiveTask<Integer> {
+
+    /**
+     * 阈值
+     */
+    private static final int THRESHOLD = 2;
+
+    private int start;
+    private int end;
+
+    public CountTask(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected Integer compute() {
+        int sum = 0;
+
+        boolean canCompute = (end - start) <= THRESHOLD;
+        if (canCompute) {
+            for (int i = start; i <= end; ++i) {
+                sum += i;
+            }
+        } else {
+            // 任务大于阈值，拆分为两个子任务
+            int middle = (start + end) / 2;
+            CountTask leftTask = new CountTask(start, middle);
+            CountTask rightTask = new CountTask(middle + 1, end);
+
+            // 执行子任务
+            leftTask.fork();
+            rightTask.fork();
+
+            // 等待子任务执行完，并得到其结果
+            int leftResult = leftTask.join();
+            int rightResult = rightTask.join();
+
+            // 汇总结果
+            sum = leftResult + rightResult;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+
+        // 生成一个计算任务，负责计算 1+2+3+4
+        CountTask task = new CountTask(1, 99999);
+
+        // 执行一个任务
+        Future<Integer> result = forkJoinPool.submit(task);
+
+        try {
+            System.out.println(result.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
