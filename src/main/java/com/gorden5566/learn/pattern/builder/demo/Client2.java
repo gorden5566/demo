d75166baf044f06b41e71785daa553fcbf1915ac
gorden5566 <gorden5566@126.com>
2019-02-03 18:20:24 +0800

    Builder director

---
+++ src/main/java/com/gorden5566/learn/pattern/builder/demo/Client2.java
@@ -0,0 +1,21 @@
+package com.gorden5566.learn.pattern.builder.demo;
+
+public class Client2 {
+    public static void main(String[] args) {
+        Director director = new Director();
+        //1辆A类型的奔驰车
+        for (int i = 0; i < 1; i++) {
+            director.getABenzModel().run();
+        }
+
+        //100辆B类型的奔驰车
+        for (int i = 0; i < 100; i++) {
+            director.getBBenzModel().run();
+        }
+
+        //1000辆C类型的宝马车
+        for (int i = 0; i < 1000; i++) {
+            director.getCBMWModel().run();
+        }
+    }
+}
