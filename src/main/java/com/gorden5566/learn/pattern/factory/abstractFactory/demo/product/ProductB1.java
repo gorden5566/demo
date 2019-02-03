9c7a57ee3f23a346e4b1751df93f30c82cfa8504
gorden5566 <gorden5566@126.com>
2019-02-03 17:04:15 +0800

    抽象工厂demo

---
+++ src/main/java/com/gorden5566/learn/pattern/factory/abstractFactory/demo/product/ProductB1.java
@@ -0,0 +1,8 @@
+package com.gorden5566.learn.pattern.factory.abstractFactory.demo.product;
+
+public class ProductB1 extends AbstractProductB {
+    @Override
+    public void doSomething() {
+        System.out.println("产品B1的实现方法");
+    }
+}
