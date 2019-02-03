bb6da2998fd53fdc66fca047f420d4bb427bb692
gorden5566 <gorden5566@126.com>
2019-02-03 15:57:24 +0800

    Lazy Init Factory Method

---
+++ src/main/java/com/gorden5566/learn/pattern/factory/factoryMethod/demo/LazyInitProductFactory.java
@@ -0,0 +1,30 @@
+package com.gorden5566.learn.pattern.factory.factoryMethod.demo;
+
+import com.gorden5566.learn.pattern.factory.factoryMethod.demo.product.ConcreteProduct1;
+import com.gorden5566.learn.pattern.factory.factoryMethod.demo.product.ConcreteProduct2;
+import com.gorden5566.learn.pattern.factory.factoryMethod.demo.product.Product;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LazyInitProductFactory {
+    private static final Map<String, Product> productMap = new HashMap<>();
+
+    public static synchronized Product createProduct(String type) {
+        Product product = null;
+
+        // 从缓存中获取实例
+        if (productMap.containsKey(type)) {
+            product = productMap.get(type);
+        } else {
+            if (type.equals("Product1")) {
+                product = new ConcreteProduct1();
+            } else {
+                product = new ConcreteProduct2();
+            }
+            productMap.put(type, product);
+        }
+
+        return product;
+    }
+}
