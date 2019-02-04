fd40d13f7c8dbf62c6941f6ad5971b63099d5981
gorden5566 <gorden5566@126.com>
2019-02-04 12:17:41 +0800

    代理模式 在真实对象的基础上进行增强

---
+++ src/main/java/com/gorden5566/learn/pattern/proxy/extend/Client.java
@@ -0,0 +1,11 @@
+package com.gorden5566.learn.pattern.proxy.extend;
+
+public class Client {
+    public static void main(String[] args) {
+        IGamePlayer player = new GamePlayer("张三");
+        IGamePlayer proxy = new GamePlayerProxy(player);
+        proxy.login("zhangsan", "password");
+        proxy.killBoss();
+        proxy.upgrade();
+    }
+}
---
+++ src/main/java/com/gorden5566/learn/pattern/proxy/extend/IProxy.java
@@ -0,0 +1,8 @@
+package com.gorden5566.learn.pattern.proxy.extend;
+
+public interface IProxy {
+    /**
+     * 计费
+     */
+    void count();
+}
