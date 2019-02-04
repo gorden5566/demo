d22239e7936df5e609dcc1ec9becfd6131f0dffe
gorden5566 <gorden5566@126.com>
2019-02-04 16:39:15 +0800

    中介者模式 demo

---
+++ src/main/java/com/gorden5566/learn/pattern/mediator/demo/colleague/ConcreteColleague2.java
@@ -0,0 +1,17 @@
+package com.gorden5566.learn.pattern.mediator.demo.colleague;
+
+import com.gorden5566.learn.pattern.mediator.demo.mediator.Mediator;
+
+public class ConcreteColleague2 extends Colleague {
+    public ConcreteColleague2(Mediator mediator) {
+        super(mediator);
+    }
+
+    public void selfMethod2() {
+        System.out.println("selfMethod2");
+    }
+
+    public void depMethod2() {
+        super.mediator.doSomething2();
+    }
+}
