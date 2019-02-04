90cec1d85a1260b30a7b77fd2c6253ee543e3826
gorden5566 <gorden5566@126.com>
2019-02-04 12:29:14 +0800

    代理模式 jdk动态代理

---
+++ src/main/java/com/gorden5566/learn/pattern/proxy/dynamic/Client.java
@@ -0,0 +1,20 @@
+package com.gorden5566.learn.pattern.proxy.dynamic;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+
+public class Client {
+    public static void main(String[] args) {
+        IGamePlayer player = new GamePlayer("张三");
+
+        InvocationHandler handler = new GamePlayIH(player);
+
+        ClassLoader classLoader = player.getClass().getClassLoader();
+        IGamePlayer proxy = (IGamePlayer) Proxy.newProxyInstance(classLoader, new Class[]{IGamePlayer.class}, handler);
+
+
+        proxy.login("zhangsan", "password");
+        proxy.killBoss();
+        proxy.upgrade();
+    }
+}
