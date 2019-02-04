21a613d6f1672c6764bda4a38e4c500a8c185448
gorden5566 <gorden5566@126.com>
2019-02-04 15:00:44 +0800

    原型模式 clone不执行构造函数

---
+++ src/main/java/com/gorden5566/learn/pattern/prototype/test/construct/Thing.java
@@ -0,0 +1,20 @@
+package com.gorden5566.learn.pattern.prototype.test.construct;
+
+public class Thing implements Cloneable {
+
+    public Thing() {
+        System.out.println("构造函数");
+    }
+
+    @Override
+    protected Thing clone() {
+        Thing thing = null;
+        try {
+            thing = (Thing) super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+
+        return thing;
+    }
+}
