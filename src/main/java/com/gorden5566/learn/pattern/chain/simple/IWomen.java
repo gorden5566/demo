8f2af92acc6d39b7b398dc4d0cc010846c51cab2
gorden5566 <gorden5566@126.com>
2019-02-05 15:39:25 +0800

    责任链模式

---
+++ src/main/java/com/gorden5566/learn/pattern/chain/simple/IWomen.java
@@ -0,0 +1,17 @@
+package com.gorden5566.learn.pattern.chain.simple;
+
+public interface IWomen {
+    /**
+     * 获得个人状况
+     *
+     * @return
+     */
+    int getType();
+
+    /**
+     * 获得个人请示， 你要干什么？ 出去逛街？ 约会?还是看电影？
+     *
+     * @return
+     */
+    String getRequest();
+}
---
+++ src/main/java/com/gorden5566/learn/pattern/chain/simple/Women.java
@@ -0,0 +1,52 @@
+package com.gorden5566.learn.pattern.chain.simple;
+
+public class Women implements IWomen {
+    /*
+     * 通过一个int类型的参数来描述妇女的个人状况
+     * 1--未出嫁
+     * 2--出嫁
+     * 3--夫死
+     */
+    private int type = 0;
+
+    /**
+     * 妇女的请示
+     */
+    private String request = "";
+
+    public Women(int type, String request) {
+        this.type = type;
+        //为了便于显示， 在这里做了点处理
+        switch (this.type) {
+            case 1:
+                this.request = "女儿的请求是： " + request;
+                break;
+            case 2:
+                this.request = "妻子的请求是： " + request;
+                break;
+            case 3:
+                this.request = "母亲的请求是： " + request;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * 获得自己的状况
+     *
+     * @return
+     */
+    public int getType() {
+        return this.type;
+    }
+
+    /**
+     * 获得妇女的请求
+     *
+     * @return
+     */
+    public String getRequest() {
+        return this.request;
+
+    }
+}
