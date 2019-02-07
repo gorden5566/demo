e0784248cde32f90c15f468d6ea565d261039dde
gorden5566 <gorden5566@126.com>
2019-02-07 16:24:42 +0800

    迭代器模式 demo

---
+++ src/main/java/com/gorden5566/learn/pattern/iterator/demo/Client.java
@@ -0,0 +1,19 @@
+package com.gorden5566.learn.pattern.iterator.demo;
+
+import com.gorden5566.learn.pattern.iterator.demo.aggregate.Aggregate;
+import com.gorden5566.learn.pattern.iterator.demo.aggregate.ConcreteAggregate;
+import com.gorden5566.learn.pattern.iterator.demo.iterator.Iterator;
+
+public class Client {
+    public static void main(String[] args) {
+        Aggregate aggregate = new ConcreteAggregate();
+        aggregate.add("abc");
+        aggregate.add("aaa");
+        aggregate.add("1234");
+
+        Iterator iterator = aggregate.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
---
+++ src/main/java/com/gorden5566/learn/pattern/iterator/demo/aggregate/Aggregate.java
@@ -0,0 +1,26 @@
+package com.gorden5566.learn.pattern.iterator.demo.aggregate;
+
+import com.gorden5566.learn.pattern.iterator.demo.iterator.Iterator;
+
+public interface Aggregate {
+    /**
+     * 是容器必然有元素的增加
+     *
+     * @param object
+     */
+    void add(Object object);
+
+    /**
+     * 减少元素
+     *
+     * @param object
+     */
+    void remove(Object object);
+
+    /**
+     * 由迭代器来遍历所有的元素
+     *
+     * @return
+     */
+    Iterator iterator();
+}
