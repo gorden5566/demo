ad62a18a027d6a84f6bdf31935f15328cbb5e99c
gorden5566 <gorden5566@126.com>
2019-02-07 16:10:15 +0800

    迭代器模式

---
+++ src/main/java/com/gorden5566/learn/pattern/iterator/simple/iterator/IProjectIterator.java
@@ -0,0 +1,6 @@
+package com.gorden5566.learn.pattern.iterator.simple.iterator;
+
+import java.util.Iterator;
+
+public interface IProjectIterator extends Iterator {
+}
---
+++ src/main/java/com/gorden5566/learn/pattern/iterator/simple/iterator/ProjectIterator.java
@@ -0,0 +1,39 @@
+package com.gorden5566.learn.pattern.iterator.simple.iterator;
+
+import com.gorden5566.learn.pattern.iterator.simple.IProject;
+
+import java.util.List;
+
+public class ProjectIterator implements IProjectIterator {
+
+    // 所有的项目都放在ArrayList中
+    private List<IProject> projectList;
+
+    // 当前位置
+    private int currentItem = 0;
+
+    public ProjectIterator(List<IProject> projectList) {
+        this.projectList = projectList;
+    }
+
+    @Override
+    public boolean hasNext() {
+        //定义一个返回值
+        boolean b = true;
+        if (this.currentItem >= projectList.size() || this.projectList.get(this.currentItem) == null) {
+            b = false;
+
+        }
+        return b;
+    }
+
+    @Override
+    public Object next() {
+        return this.projectList.get(this.currentItem++);
+    }
+
+    @Override
+    public void remove() {
+
+    }
+}
