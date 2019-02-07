a1be9bc221faa0f843cd3775abb8c0f407b12b9a
gorden5566 <gorden5566@126.com>
2019-02-07 14:30:40 +0800

    策略模式 demo

---
+++ src/main/java/com/gorden5566/learn/pattern/strategy/demo/Client.java
@@ -0,0 +1,18 @@
+package com.gorden5566.learn.pattern.strategy.demo;
+
+import com.gorden5566.learn.pattern.strategy.demo.strategy.ConcreteStrategy1;
+import com.gorden5566.learn.pattern.strategy.demo.strategy.ConcreteStrategy2;
+import com.gorden5566.learn.pattern.strategy.demo.strategy.Strategy;
+
+public class Client {
+    public static void main(String[] args) {
+        Strategy strategy1 = new ConcreteStrategy1();
+        Strategy strategy2 = new ConcreteStrategy2();
+
+        Context context = new Context(strategy1);
+        context.doAnything();
+
+        context.setStrategy(strategy2);
+        context.doAnything();
+    }
+}
---
+++ src/main/java/com/gorden5566/learn/pattern/strategy/demo/strategy/Strategy.java
@@ -0,0 +1,9 @@
+package com.gorden5566.learn.pattern.strategy.demo.strategy;
+
+public interface Strategy {
+
+    /**
+     * 策略模式的运算法
+     */
+    void doSomething();
+}
