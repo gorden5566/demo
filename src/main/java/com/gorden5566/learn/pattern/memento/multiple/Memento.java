61e73bf64e2b8ccf7f6d1a820b86b0335fcb128c
gorden5566 <gorden5566@126.com>
2019-02-09 17:14:19 +0800

    备忘录模式 多状态

---
+++ src/main/java/com/gorden5566/learn/pattern/memento/multiple/Memento.java
@@ -0,0 +1,19 @@
+package com.gorden5566.learn.pattern.memento.multiple;
+
+import java.util.Map;
+
+public class Memento {
+    private Map<String, Object> stateMap;
+
+    public Memento(Map<String, Object> stateMap) {
+        this.stateMap = stateMap;
+    }
+
+    public Map<String, Object> getStateMap() {
+        return stateMap;
+    }
+
+    public void setStateMap(Map<String, Object> stateMap) {
+        this.stateMap = stateMap;
+    }
+}
