4d044c11b42f5dbc44c8707037625ca67e0f8620
gorden5566 <gorden5566@126.com>
2019-02-12 22:36:24 +0800

    解释器模式

---
+++ src/main/java/com/gorden5566/learn/pattern/interpreter/ReadMe.md
@@ -0,0 +1,5 @@
+## description
+Given a language, define a representation for its grammar along with an interpreter that uses the representation to interpret sentences in the language.
+
+## 说明
+给定一门语言，定义它的文法的一种表示，并定义一个解释器，该解释器使用该表示来解释语言中的句子。
---
+++ src/main/java/com/gorden5566/learn/pattern/interpreter/simple/expression/symbol/SubExpression.java
@@ -0,0 +1,22 @@
+package com.gorden5566.learn.pattern.interpreter.simple.expression.symbol;
+
+import com.gorden5566.learn.pattern.interpreter.simple.expression.Expression;
+
+import java.util.Map;
+
+/**
+ * 减号解析器
+ *
+ * @author gorden5566
+ * @date 2019-02-12
+ */
+public class SubExpression extends SymbolExpression {
+    public SubExpression(Expression left, Expression right) {
+        super(left, right);
+    }
+
+    @Override
+    public int interpreter(Map<String, Integer> var) {
+        return super.left.interpreter(var) - super.right.interpreter(var);
+    }
+}
