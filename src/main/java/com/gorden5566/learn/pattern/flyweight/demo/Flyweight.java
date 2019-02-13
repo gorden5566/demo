98d208501bafd3d947eebf92487aef96a08b44d8
gorden5566 <gorden5566@126.com>
2019-02-13 12:20:22 +0800

    享元模式 demo

---
+++ src/main/java/com/gorden5566/learn/pattern/flyweight/demo/Flyweight.java
@@ -0,0 +1,35 @@
+package com.gorden5566.learn.pattern.flyweight.demo;
+
+/**
+ * @author gorden5566
+ * @date 2019-02-13
+ */
+public abstract class Flyweight {
+
+    /**
+     * 内部状态
+     */
+    private String intrinsic;
+
+    /**
+     * 外部状态
+     */
+    protected final String Extrinsic;
+
+    public Flyweight(String extrinsic) {
+        Extrinsic = extrinsic;
+    }
+
+    /**
+     * 定义业务操作
+     */
+    public abstract void operate();
+
+    public String getIntrinsic() {
+        return intrinsic;
+    }
+
+    public void setIntrinsic(String intrinsic) {
+        this.intrinsic = intrinsic;
+    }
+}
