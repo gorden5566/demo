05b51f412627d8f547e0226c06f24601d443813b
gorden5566 <gorden5566@126.com>
2019-02-20 13:57:34 +0800

    UseCharsets

---
+++ src/main/java/com/gorden5566/demos/io/nio/UseCharsets.java
@@ -0,0 +1,50 @@
+package com.gorden5566.demos.io.nio;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
+
+/**
+ * @author gorden5566
+ * @date 2019-02-20
+ */
+public class UseCharsets {
+    public static void main(String[] args) throws IOException {
+        String path = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+
+        String inputFile = path + "log4j.properties";
+        String outputFile = path + "sampleout.txt";
+
+        RandomAccessFile inf = new RandomAccessFile(inputFile, "r");
+        RandomAccessFile outf = new RandomAccessFile(outputFile, "rw");
+        long inputLength = new File(inputFile).length();
+
+        FileChannel inc = inf.getChannel();
+        FileChannel outc = outf.getChannel();
+
+        // 输入文件的内存映射
+        MappedByteBuffer inputData = inc.map(FileChannel.MapMode.READ_ONLY, 0, inputLength);
+
+        Charset latin1 = Charset.forName("ISO-8859-1");
+        CharsetDecoder decoder = latin1.newDecoder();
+        CharsetEncoder encoder = latin1.newEncoder();
+
+        CharBuffer cb = decoder.decode(inputData);
+
+        // Process char data here
+
+        ByteBuffer outputData = encoder.encode(cb);
+
+        outc.write(outputData);
+
+        inf.close();
+        outf.close();
+    }
+}
