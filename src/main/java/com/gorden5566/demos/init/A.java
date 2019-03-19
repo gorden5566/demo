de6597ceb39330df5e954ec6689673f7595b4f69
gorden5566 <gorden5566@126.com>
2019-03-19 11:11:51 +0800

    init顺序

---
+++ src/main/java/com/gorden5566/demos/init/A.java
@@ -0,0 +1,42 @@
+package com.gorden5566.demos.init;
+
+/**
+ * @author gorden5566
+ * @date 2019-03-04
+ */
+public class A {
+
+    int a1 = 8;
+    int a2 = getA2();
+    {
+        int a3 = 9;
+        System.out.println("top of A() a1=" + a1 + " a2=" + a2 + "  a3=" + a3);
+    }
+
+    public A() {
+        this(66);
+        System.out.print("A 构造函数\n");
+    }
+
+    {
+        System.out.println("below A()..has start");
+    }
+
+    public A(int num) {
+        System.out.print("A 带参数构造函数: " + num + "\n");
+    }
+
+    static {
+        System.out.println("I`m a static {} from class A..");
+    }
+
+    int getA2() {
+        System.out.println("getA2..");
+        return 7;
+    }
+
+    public void methodA() {
+        System.out.println("methodA");
+    }
+
+}
---
+++ src/main/java/com/gorden5566/demos/init/Test.java
@@ -0,0 +1,16 @@
+package com.gorden5566.demos.init;
+
+/**
+ * @author gorden5566
+ * @date 2019-03-04
+ */
+public class Test {
+    public static void main(String[] args) {
+        B b = new B();
+        // B b = new B(22);
+        b.methodA();
+
+//        A a = new A();
+//        System.out.println();
+    }
+}
