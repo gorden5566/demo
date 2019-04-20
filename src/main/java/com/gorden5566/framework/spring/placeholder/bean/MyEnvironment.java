068fece78bb743a7cf95638114699fb88ba2d07c
gorden5566 <gorden5566@126.com>
2019-04-20 10:22:20 +0800

    PropertyPlaceholderConfigurer

---
+++ src/main/java/com/gorden5566/framework/spring/placeholder/bean/MyEnvironment.java
@@ -0,0 +1,17 @@
+package com.gorden5566.framework.spring.placeholder.bean;
+
+/**
+ * @author gorden5566
+ * @date 2019/04/20
+ */
+public class MyEnvironment {
+    private String appName;
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+}
