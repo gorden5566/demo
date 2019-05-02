3c2b1559c7ac20040a186930539473817b09a383
gorden5566 <gorden5566@126.com>
2019-05-02 11:58:29 +0800

    binary tree

--- src/main/java/com/gorden5566/algorithm/tree/RebuildBinaryTree.java
+++ src/main/java/com/gorden5566/algorithm/tree/RebuildBinaryTree.java
@@ -2,6 +2,8 @@ package com.gorden5566.algorithm.tree;
 
 import com.gorden5566.algorithm.util.TreeNode;
 
+import java.util.ArrayList;
+
 /**
  * 题目描述
  * <p>
@@ -13,6 +15,15 @@ import com.gorden5566.algorithm.util.TreeNode;
  * @date 2019-03-06
  */
 public class RebuildBinaryTree {
+
+    public static void main(String[] args) {
+        int[] pre = {1,2,4,7,3,5,6,8};
+        int[] in = {4,7,2,1,5,3,8,6};
+
+        TreeNode node = reConstructBinaryTree(pre, in);
+        ArrayList<Integer> integers = PrintFromTopToBottom.PrintFromTopToBottom(node);
+        System.out.println(integers);
+    }
 
     public static TreeNode reConstructBinaryTree(int[] pre, int[] in) {
         if (pre == null || in == null || pre.length == 0 || in.length == 0) {
