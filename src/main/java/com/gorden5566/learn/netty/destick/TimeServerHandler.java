54a889b217841ca07f282872770f43cb54f617e5
gorden5566 <gorden5566@126.com>
2019-07-20 13:09:26 +0800

    resolve stick package

---
+++ src/main/java/com/gorden5566/learn/netty/destick/TimeServerHandler.java
@@ -0,0 +1,34 @@
+package com.gorden5566.learn.netty.destick;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+import java.util.Date;
+
+/**
+ * @author gorden5566
+ * @date 2019/07/20
+ */
+public class TimeServerHandler extends ChannelHandlerAdapter {
+
+    private int counter;
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        String body = (String) msg;
+        System.out.println("receive order: " + body + ", counter: " + ++counter);
+
+        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+        currentTime = currentTime + System.getProperty("line.separator");
+
+        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+        ctx.writeAndFlush(resp);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+}
