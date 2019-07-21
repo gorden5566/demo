cd5f644e2f511bfa9ccada8dcb521092a88fc94b
gorden5566 <gorden5566@126.com>
2019-07-21 21:50:30 +0800

    http server

---
+++ src/main/java/com/gorden5566/learn/netty/http/HttpFileServer.java
@@ -0,0 +1,66 @@
+package com.gorden5566.learn.netty.http;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * @author gorden5566
+ * @date 2019/07/21
+ */
+public class HttpFileServer {
+    private static final String DEFAULT_URL = "/src/main/java/com/gorden5566/learn/netty";
+
+    public void run(final int port, final String url) throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ch.pipeline().addLast("http-decoder", new HttpRequestDecoder());
+                        ch.pipeline().addLast("http-aggregator", new HttpObjectAggregator(65536));
+                        ch.pipeline().addLast("http-encoder", new HttpResponseEncoder());
+                        ch.pipeline().addLast("http-chunked", new ChunkedWriteHandler());
+                        ch.pipeline().addLast("fileServerHandler", new HttpFileServerHandler(url));
+                    }
+                });
+
+            ChannelFuture future = bootstrap.bind("127.0.0.1", port).sync();
+            System.out.println("http file server started, http://127.0.0.1:" + port + url);
+            future.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+            }
+        }
+
+        String url = DEFAULT_URL;
+        if (args.length > 1) {
+            url = args[1];
+        }
+
+        new HttpFileServer().run(port, url);
+    }
+}
