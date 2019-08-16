5d48275f22dcc540f97a5d456465d08ff22df8a5
gorden5566 <gorden5566@126.com>
2019-08-16 22:17:48 +0800

    [jetty] servlet

---
+++ src/main/java/com/gorden5566/learn/jetty/servlet/MinimalServlets.java
@@ -0,0 +1,40 @@
+package com.gorden5566.learn.jetty.servlet;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author gorden5566
+ * @date 2019/08/16
+ */
+public class MinimalServlets {
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+
+        ServletHandler handler = new ServletHandler();
+        server.setHandler(handler);
+
+        handler.addServletWithMapping(HelloServlet.class, "/*");
+
+        server.start();
+
+        server.join();
+    }
+
+    @SuppressWarnings("serial")
+    public static class HelloServlet extends HttpServlet {
+        @Override
+        protected void doGet(HttpServletRequest request,
+                             HttpServletResponse response) throws ServletException, IOException {
+            response.setContentType("text/html");
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.getWriter().println("<h1>Hello from HelloServlet</h1>");
+        }
+    }
+}
