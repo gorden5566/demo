0243cefce8c641aa3f58bd6104c548396986a687
gorden5566 <gorden5566@126.com>
2019-09-01 16:01:24 +0800

    [netty] simple servlet mapping

--- src/main/java/com/gorden5566/learn/netty/servlet/DefaultHttpServletRequest.java
+++ src/main/java/com/gorden5566/learn/netty/servlet/DefaultHttpServletRequest.java
@@ -10,10 +10,7 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.InetSocketAddress;
 import java.security.Principal;
-import java.util.Collection;
-import java.util.Enumeration;
-import java.util.Locale;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author gorden5566
@@ -57,12 +54,12 @@ public class DefaultHttpServletRequest implements HttpServletRequest {
 
     @Override
     public Enumeration<String> getHeaderNames() {
-        return null;
+        return Collections.enumeration(request.headers().names());
     }
 
     @Override
     public int getIntHeader(String name) {
-        return 0;
+        return request.headers().getInt(name);
     }
 
     @Override
