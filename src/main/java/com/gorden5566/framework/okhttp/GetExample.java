6757707ccac1327d58291b0f10fd7754ab65dd82
gorden5566 <gorden5566@126.com>
2020-05-05 12:28:01 +0800

    okhttp

---
+++ src/main/java/com/gorden5566/framework/okhttp/GetExample.java
@@ -0,0 +1,35 @@
+package com.gorden5566.framework.okhttp;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+/**
+ * @author renjianjun
+ * @date 2019/12/27
+ */
+public class GetExample {
+    private OkHttpClient client = new OkHttpClient();
+
+    public String run(String url) throws IOException {
+        Request request = new Request.Builder()
+            .url(url)
+            .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+    private String test() {
+        return "aaa";
+    }
+
+    public static void main(String[] args) throws IOException {
+        GetExample example = new GetExample();
+        String response = example.run("https://raw.github.com/square/okhttp/master/README.md");
+        System.out.println(response);
+    }
+}
