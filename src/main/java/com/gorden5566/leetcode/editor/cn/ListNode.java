f9f970f6055284c19e2266768bf3c1865a233075
gorden5566 <gorden5566@126.com>
2021-01-12 00:45:01 +0800

    ReverseLinkedList

---
+++ src/main/java/com/gorden5566/leetcode/editor/cn/ListNode.java
@@ -0,0 +1,14 @@
+package com.gorden5566.leetcode.editor.cn;
+
+/**
+ * @author gorden5566
+ * @date 2021/01/12
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
