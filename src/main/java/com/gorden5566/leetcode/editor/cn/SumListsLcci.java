b281a6f7aea6583da5f00a3161d9029c1f1a2b04
gorden5566 <gorden5566@126.com>
2021-01-13 00:38:19 +0800

    SumListsLcci

--- src/main/java/com/gorden5566/leetcode/editor/cn/SumListsLcci.java
+++ src/main/java/com/gorden5566/leetcode/editor/cn/SumListsLcci.java
@@ -38,7 +38,58 @@ public class SumListsLcci{
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return null;
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return null;
+        }
+
+        ListNode head = new ListNode(Integer.MIN_VALUE);
+        ListNode p = head;
+        ListNode p1 = l1;
+        ListNode p2 = l2;
+
+        int carry = 0;
+        while (p1 != null && p2 != null) {
+            int sum = p1.val + p2.val + carry;
+            int current = sum % 10;
+            carry = sum / 10;
+
+            p.next = new ListNode(current);
+
+            p = p.next;
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        if (p1 != null) {
+            while (p1 != null) {
+                int sum = p1.val + carry;
+                int current = sum % 10;
+                carry = sum / 10;
+
+                p.next = new ListNode(current);
+                p = p.next;
+                p1 = p1.next;
+            }
+        } else if (p2 != null) {
+            while (p2 != null) {
+                int sum = p2.val + carry;
+                int current = sum % 10;
+                carry = sum / 10;
+
+                p.next = new ListNode(current);
+                p = p.next;
+                p2 = p2.next;
+            }
+        }
+
+        if (carry > 0) {
+            p.next = new ListNode(carry);
+        }
+
+        return head.next;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
